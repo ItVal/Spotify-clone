e5719b06ea147f997c534456870b6a5ddd2527d6
ItVal <valentinhonvj1@gmail.com>
2022-08-29 17:21:52 +0200

    style : Login component

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
-import { FcGoogle } from 'react-icons/fc';
+import { ImGoogle3 } from 'react-icons/im';
 import { SiFacebook } from 'react-icons/si'; 
 import { BsGithub } from 'react-icons/bs';
 
 const Login = () => {
   return (
-    <div>
+    <div className='login'>
         <h1 className='loginTitle'> Choose a Login Method</h1>
         <div className="wrapper">
             <div className="left">
-                <div className="loginButton">
-                 <h3> <FcGoogle /> Google</h3>
+                <div className="loginButton google">
+                 <h3 className='icon'> <ImGoogle3 /> </h3>
+                 <span>Google</span>
                 </div>
-                <div className="loginButton">
-                <h3> <SiFacebook /> Facebook</h3>
+                <div className="loginButton facebook">
+                <h3 className='icon'> <SiFacebook /> </h3>
+                <span>Facebook</span>
                 </div>
-                <div className="loginButton">
-                <h3> <BsGithub />Github</h3>
+                <div className="loginButton github">
+                <h3 className='icon'> <BsGithub /></h3>
+                <span>Github</span>
                 </div>
+            </div>
+            <div className="center">
+                <div className="line"/>
+                <div className="or">OR</div>
             </div>
             <div className="right">
                 <input type="text" placeholder='Username' />
