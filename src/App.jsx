95e17c7c5e5ba6401be3e7940074acc9069d71cd
ItVal <valentinhonvj1@gmail.com>
2022-08-30 00:52:26 +0200

    feat : add BowserRoute, Routes and Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,24 @@ import Navbar from './Components/Navbar'
 import Login from './Pages/Login'
 import Home from './Pages/Home'
 import Post from './Pages/Post'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 
 
 function App() {
- 
+ const user = false;
   return (
+    <BrowserRouter>
     <div className="App">
     <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={user ? <Navigate to="/" /> : <Login />} />
+      <Route path='/post/:id' element={user ? <Post /> : <Navigate to='/login' />} />
+    </Routes>
     <Login />
     </div>
+    </BrowserRouter>
   )
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import '../assets/style.css'
 import Imgprofil from '../assets/Val.jpeg'
 
-const Navbar = () => {
+const Navbar = ({user}) => {
   return (
     <div className="navbar">
-      <span className="logo">YabisoNdule</span>
-      <ul className="list">
+      <span className="logo"><Link className="link" to="/">Yabiso Ndule</Link></span>
+      {user ? (
+        <ul className="list">
         <li className="listItem">
           <img src={Imgprofil} alt="" className="avatar" />
         </li>
         <li className="listItem">Nasibu valentin</li>
         <li className="listItem">Logout</li>
       </ul>
+        ) : (<Link className="link" to="login">Login</Link>)}
+        
     </div>
   );
 };
